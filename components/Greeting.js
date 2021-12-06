4ae2c8168439fe782de48c3b65b02de6eb529116
jihye <jiji.pak41@gmail.com>
2021-12-06 16:03:40 +0000

    create mycourses page - landing
    <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Elena <76445935+elenamarinaki@users.noreply.github.com>
    Co-authored-by: Danilo <81513148+danilo-cupido@users.noreply.github.com>
    Co-authored-by: moggach <43313455+Moggach@users.noreply.github.com>
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>

---
+++ components/Greeting.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { CgProfile } from 'react-icons/Cg';
+
+const Greeting = () => {
+  return (
+    <div>
+      <CgProfile />
+      <p>Hello User!</p>
+      <p>My courses</p>
+    </div>
+  );
+};
+
+export default Greeting;
