5e6d9aa67dbd0eb85bfd869085a16141b4bc9f01
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2021-12-08 16:03:57 +0000

    social media icons appear in the middle of the page they're positioned in flex with justify-between property

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { FaInstagram, FaTwitterSquare, FaFacebookSquare } from 'react-icons/fa';
 
 const contact = () => {
   return (
@@ -25,16 +26,28 @@ const contact = () => {
         </p>
       </section>
 
-      <section className="flex flex-col justify-center m-auto w-96 bg-PURPLE rounded-lg text-WHITE mt-8">
+      <section className="flex flex-col justify-center m-auto w-96 bg-PURPLE rounded-md text-WHITE mt-8">
         <p className="m-4">Telephone: 023 8017 9049 </p>
         <p className="m-4">Weekdays: 9am-7pm </p>
         <p className="m-4">Weekends: 10am-2pm </p>
       </section>
 
-      <section className="flex flex-col justify-center m-auto w-96 border-2 border-PURPLE shadow-md rounded-lg mt-8">
+      <section className="flex flex-col justify-center m-auto w-96 border-2 border-PURPLE shadow-lg rounded-md mt-8">
         <p className="m-4">For all other general, administrative enquires:</p>
         <p className="m-4">Telephone: 023 8202 7810</p>
         <p className="m-4">Email: info@solentmind.org.uk *</p>
+      </section>
+
+      <section className="flex justify-center mt-4">
+        <p className="text-sm">
+          *Please do not send any confidential information to us via email.
+        </p>
+      </section>
+
+      <section className="flex justify-between w-96 mt-4 m-auto">
+        <FaTwitterSquare size={32} style={{ color: '#5F55B9' }} />
+        <FaInstagram size={32} style={{ color: '#5F55B9' }} />
+        <FaFacebookSquare size={32} style={{ color: '#5F55B9' }} />
       </section>
     </div>
   );
