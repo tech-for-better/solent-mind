84a0665aec3c75563dd31cbbc858ef627effabd9
Anna Cunnane <43313455+Moggach@users.noreply.github.com>
2021-12-08 11:48:13 +0000

    Update Greeting.js
    Removing unused code and comment

--- components/Greeting.js
+++ components/Greeting.js
@@ -3,12 +3,11 @@ import { CgProfile } from 'react-icons/cg';
 import { supabase } from '../utils/supabaseClient';
 
 const Greeting = () => {
-  const user = supabase.auth.user();
+ 
 
   return (
     <div>
       <CgProfile />
-      {/* <p>Hello {user.email}</p> */}
       <p>My courses</p>
     </div>
   );
