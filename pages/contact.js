cc1028840146086a0d3a48f0ab417ef763fe673f
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2021-12-08 15:12:00 +0000

    solent mind logo appears in the center

--- pages/contact.js
+++ pages/contact.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import Image from 'next/image';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
 const contact = () => {
   return (
     <div>
       <div className="flex flex-row place-content-end p-4">
-        <GiHamburgerMenu size={32} style={{color: '#B01A76'}}/>
+        <GiHamburgerMenu size={32} style={{ color: '#B01A76' }} />
       </div>
 
-
+      <div className="flex justify-center">
+        <Image
+          src="/logo/solent.png"
+          width={400}
+          height={100}
+          alt="solent mind organization logo"
+        />
+      </div>
     </div>
   );
 };
