1233089a2c671fa381fa79e42251e4128e088921
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2021-12-08 23:05:17 +0000

    side menu - appears on top of the page and slides into the page after we press the burger menu

---
+++ components/SideMenu.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Link from 'next/link';
+import { FaInstagram, FaTwitterSquare, FaFacebookSquare } from 'react-icons/fa';
+import { IoIosClose } from 'react-icons/Io';
+
+export const SideMenu = ({ setOpen }) => {
+  return (
+    <div className="flex flex-col place-content-end h-screen w-80 bg-DARKPINK bg-opacity-90 rounded-l-lg text-WHITE">
+      <section className="flex flex-row place-content-end p-2">
+        <IoIosClose
+          onClick={() => {
+            setOpen(false);
+          }}
+          size={48}
+          className={
+            open
+              ? 'transform rotate-0 transition ease-in'
+              : 'transform rotate-45 transition ease-in' + ' cursor-pointer'
+          }
+        />
+      </section>
+      <section className="flex flex-col p-6 space-y-8 text-lg">
+        <Link href="">
+          <a>Profile</a>
+        </Link>
+        <Link href="">
+          <a>My Courses</a>
+        </Link>
+        <Link href="">
+          <a>Book Course</a>
+        </Link>
+        <Link href="">
+          <a>Resources</a>
+        </Link>
+        <Link href="">
+          <a>About Us</a>
+        </Link>
+        <Link href="">
+          <a>My Progress</a>
+        </Link>
+        <Link href="">
+          <a>Contact</a>
+        </Link>
+      </section>
+
+      <section className="flex justify-between w-48 mt-12 m-auto">
+        <FaTwitterSquare size={32} />
+        <FaInstagram size={32} />
+        <FaFacebookSquare size={32} />
+      </section>
+    </div>
+  );
+};
--- pages/contact.js
+++ pages/contact.js
@@ -1,69 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { FaInstagram, FaTwitterSquare, FaFacebookSquare } from 'react-icons/fa';
+import { SideMenu } from '../components/SideMenu';
 
-const contact = () => {
+const Contact = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="font-sans">
-      <div className="flex flex-row place-content-end p-4">
-        <GiHamburgerMenu
-          className="cursor-pointer"
-          size={32}
-          style={{ color: '#B01A76' }}
-        />
+      <div className="relative">
+        <div className="flex flex-row place-content-end p-4">
+          <GiHamburgerMenu
+            onClick={() => {
+              setOpen(true);
+            }}
+            className={
+              open
+                ? 'transform rotate-90 transition ease-in'
+                : 'transform rotate-0' + ' cursor-pointer'
+            }
+            size={32}
+            style={open ? { color: '#FFFFFF' } : { color: '#B01A76' }}
+          />
+        </div>
+
+        <section className="flex justify-center">
+          <Image
+            src="/logo/solent.png"
+            width={400}
+            height={100}
+            alt="solent mind organization logo"
+          />
+        </section>
+
+        <div className="flex flex-col m-auto sm:w-2/3 lg:w-3/5">
+          <section className="flex justify-center">
+            <p>
+              If you feel low, anxious or need someone to talk to, speak to
+              trained mental health advisors through our support line:
+            </p>
+          </section>
+
+          <section className="flex flex-col justify-center m-auto w-96 bg-PURPLE rounded-md shadow-lg text-WHITE mt-8">
+            <p className="m-4">
+              Telephone: <span className="underline">023 8017 9049</span>
+            </p>
+            <p className="m-4">
+              Weekdays: 9<span className="text-sm">am</span>-7
+              <span className="text-sm">pm</span>
+            </p>
+            <p className="m-4">
+              Weekends: 10<span className="text-sm">am</span>-2
+              <span className="text-sm">pm</span>{' '}
+            </p>
+          </section>
+
+          <section className="flex flex-col justify-center m-auto w-96 border-2 border-PURPLE shadow-lg rounded-md mt-8">
+            <p className="text-sm m-4">
+              For all other general, administrative enquires:
+            </p>
+            <p className="m-4">Telephone: 023 8202 7810</p>
+            <p className="m-4">
+              Email: <span className="underline">info@solentmind.org.uk</span> *
+            </p>
+          </section>
+
+          <section className="flex justify-center mt-4">
+            <p className="text-xs">
+              *Please do not send any confidential information to us via email.
+            </p>
+          </section>
+
+          <section className="flex justify-between w-96 mt-4 m-auto">
+            <FaTwitterSquare size={32} style={{ color: '#5F55B9' }} />
+            <FaInstagram size={32} style={{ color: '#5F55B9' }} />
+            <FaFacebookSquare size={32} style={{ color: '#5F55B9' }} />
+          </section>
+
+          <section className="flex flex-col mt-8">
+            <p>Solent Mind</p>
+            <p>15-16 The Avenue</p>
+            <p>Southampton</p>
+            <p>SO17 1XF</p>
+          </section>
+        </div>
       </div>
 
-      <section className="flex justify-center">
-        <Image
-          src="/logo/solent.png"
-          width={400}
-          height={100}
-          alt="solent mind organization logo"
-        />
-      </section>
-
-      <div className="flex flex-col m-auto sm:w-2/3 lg:w-3/5">
-        <section className="flex justify-center">
-          <p>
-            If you feel low, anxious or need someone to talk to, speak to
-            trained mental health advisors through our support line:
-          </p>
-        </section>
-
-        <section className="flex flex-col justify-center m-auto w-96 bg-PURPLE rounded-md shadow-lg text-WHITE mt-8">
-          <p className="m-4">Telephone: 023 8017 9049 </p>
-          <p className="m-4">Weekdays: 9am-7pm </p>
-          <p className="m-4">Weekends: 10am-2pm </p>
-        </section>
-
-        <section className="flex flex-col justify-center m-auto w-96 border-2 border-PURPLE shadow-lg rounded-md mt-8">
-          <p className="m-4">For all other general, administrative enquires:</p>
-          <p className="m-4">Telephone: 023 8202 7810</p>
-          <p className="m-4">Email: info@solentmind.org.uk *</p>
-        </section>
-
-        <section className="flex justify-center mt-4">
-          <p className="text-sm">
-            *Please do not send any confidential information to us via email.
-          </p>
-        </section>
-
-        <section className="flex justify-between w-96 mt-4 m-auto">
-          <FaTwitterSquare size={32} style={{ color: '#5F55B9' }} />
-          <FaInstagram size={32} style={{ color: '#5F55B9' }} />
-          <FaFacebookSquare size={32} style={{ color: '#5F55B9' }} />
-        </section>
-
-        <section className="flex flex-col mt-8">
-          <p>Solent Mind</p>
-          <p>15-16 The Avenue</p>
-          <p>Southampton</p>
-          <p>SO17 1XF</p>
-        </section>
+      {/* SLIDE MENU */}
+      <div
+        className={
+          open
+            ? 'transform translate-x-1/5 fixed top-0 right-0 transition ease-in'
+            : 'transform translate-x-full absolute top-0 right-0 transition ease-in'
+        }
+      >
+        <SideMenu setOpen={setOpen} />
       </div>
     </div>
   );
 };
 
-export default contact;
+export default Contact;
