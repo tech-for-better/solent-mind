33c2c55b9cff2a69752677d2dccc53ad5981e726
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2021-12-08 15:02:47 +0000

    burger menu appears correctly on page it's placed on the right, and it's dark pink the size is also changed

---
+++ pages/contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
+const contact = () => {
+  return (
+    <div>
+      <div className="flex flex-row place-content-end p-4">
+        <GiHamburgerMenu size={32} style={{color: '#B01A76'}}/>
+      </div>
+
+
+    </div>
+  );
+};
+
+export default contact;
