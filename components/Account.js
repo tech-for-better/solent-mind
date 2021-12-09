2a517ef3adbbae6672f35734b4c99369046d3b0b
jihye <jiji.pak41@gmail.com>
2021-12-09 17:49:35 +0000

    present user name and profile photo
    Co-authored-by: moggach <43313455+Moggach@users.noreply.github.com>

--- components/Account.js
+++ components/Account.js
@@ -3,76 +3,24 @@ import { supabase } from '../utils/supabaseClient';
 import Image from 'next/image';
 
 export default function Account({ userProfile, setUserProfile }) {
-  const [loading, setLoading] = useState(true);
-  const [email, setEmail] = useState(null);
-  const [avatar, setAvatar] = useState(null);
-  const [username, setUsername] = useState(null);
-  const [loggedinAt, setLoggedinAt] = useState(null);
-
-  // useEffect(() => {
-  //   getProfile();
-  // }, []);
-
-  // async function getProfile() {
-  //   try {
-  //     setLoading(true);
-  //     const user = supabase.auth.user();
-
-  //     let { data, error, status } = await supabase
-  //       .from('profiles')
-  //       .select('username, avatar')
-  //       .eq('id', user.id)
-  //       .single();
-
-  //     if (error && status !== 406) {
-  //       throw error;
-  //     }
-
-  //     if (data) {
-  //       setUsername(data.username);
-  //       setAvatar(data.avatar);
-  //     }
-  //   } catch (error) {
-  //     alert(error.message);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // }
-
-  // async function updateProfile({ username }) {
-  //   try {
-  //     setLoading(true);
-  //     const user = supabase.auth.user();
-
-  //     const updates = {
-  //       id: profiles.id,
-  //       username,
-  //     };
-
-  //     let { error } = await supabase.from('users').upsert(updates, {
-  //       returning: 'minimal', // Don't return the value after inserting
-  //     });
-
-  //     if (error) {
-  //       throw error;
-  //     }
-  //   } catch (error) {
-  //     alert(error.message);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // }
-  console.log('userProfile on Account :', userProfile);
   return (
     <>
-      Hello
-      {/* <div>Welcome {username}!</div>
-      <div>You last signed in at</div>
-      {/* <Image src={avatar} alt={username} width={500} height={500} /> */}
-      {/* <div>
-        <button onClick={() => supabase.auth.signOut()}>Sign Out</button>
-      </div> */}{' '}
-      */
+      <div>
+        {userProfile === null ? (
+          'Loding'
+        ) : (
+          <div>
+            <p>Welcome {userProfile.username}</p>{' '}
+            <Image
+              src={userProfile.avatar}
+              alt={`image of ${userProfile.username}`}
+              width={500}
+              height={500}
+            />
+            <button onClick={() => supabase.auth.signOut()}>Sign Out</button>
+          </div>
+        )}
+      </div>
     </>
   );
 }
