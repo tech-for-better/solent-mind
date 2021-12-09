dc0b95b673e9c74c27469b121d91f6206078d5a0
MJOW1999 <michael.jow.williams@gmail.com>
2021-12-09 10:46:12 +0000

    Merge branch

--- components/Greeting.js
+++ components/Greeting.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { CgProfile } from 'react-icons/cg';
 import { supabase } from '../utils/supabaseClient';
 
-const Greeting = () => {
+const Greeting = ({ user }) => {
   return (
-    <div>
-      <CgProfile />
-      <p>My courses</p>
+    <div className="flex flex-row items-center justify-between bg-PURPLE h-12 pl-4 pr-14">
+      <div className="rounded-full inline-block bg-WHITE text-DARKPINK mt-10 text-5xl	">
+        <CgProfile />
+      </div>
+      <p className="text-WHITE">Hello{user}</p>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      PURPLE: '#5F55B9',
+      BLUE: '#006ECA',
+      DARKPINK: '#B01A76',
+      YELLOW: '#FFB703',
+      WHITE: '#FEFEFE'
+
+    },
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+      // sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   variants: {
