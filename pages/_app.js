20b87c091840e8e4f553a2a325d7d2b8cb2456b9
“Moggach” <acunnane13@gmail.com>
2021-12-13 10:27:45 +0000

    defining session in app.js
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Elena <76445935+elenamarinaki@users.noreply.github.com>
    Co-authored-by: Danilo <81513148+danilo-cupido@users.noreply.github.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import 'tailwindcss/tailwind.css';
-import { supabase, session } from '../utils/supabaseClient';
+import { useSupabase, supabase } from '../utils/supabaseClient';
 
 function MyApp({ Component, pageProps }) {
-  // const { session, supabase } = useSupabase();
+  const session = useSupabase();
   return <Component session={session} supabase={supabase} {...pageProps} />;
 }
 
