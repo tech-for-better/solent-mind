0599d9c016b4b2f5b7dab1a6b6cebd94d5f8d8f6
“Moggach” <acunnane13@gmail.com>
2021-12-15 11:58:48 +0000

    adding form for updating details on profile page
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>

--- pages/myProfile.js
+++ pages/myProfile.js
@@ -48,11 +48,48 @@ const MyProfile = ({ supabase, session }) => {
             <Greeting user={userData ? ` ${userData.email}` : 'User'} />
             <PageHeader>My Profile</PageHeader>
 
+            <form>
+              <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/3">
+                  <label
+                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                    htmlFor="inline-full-name"
+                  >
+                    Username
+                  </label>
+                </div>
+                <div className="md:w-2/3">
+                  <input
+                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                    id="inline-full-name"
+                    type="text"
+                    placeholder="Your username"
+                  ></input>
+                </div>
+              </div>
+              <label className="button primary block" htmlFor="single">
+                Upload a profile picture
+              </label>
+              <input type="file" id="single" accept="image/*" />
+              <div className="md:flex md:items-center">
+                <div className="md:w-1/3"></div>
+                <div className="md:w-2/3">
+                  <button
+                    className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                    type="button"
+                  >
+                    Update
+                  </button>
+                </div>
+              </div>
+            </form>
+
+            {/* 
             <Account
               userProfile={userProfile}
               setUserProfile={setUserProfile}
               userData={userData}
-            />
+            /> */}
 
             <div className="bg-PURPLE shadow-md"></div>
             <Tabs contents={contents} />
